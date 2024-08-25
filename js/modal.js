f50f465b3e83211b5a0bec2099e0e330c1df6ff6
Ivan Shevtsov <vanyashevcov@ukr.net>
2024-08-25 09:26:35 +0300

    Update adaptive code

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,24 @@
       refs.modal.classList.toggle("is-open");
     }
   })();
+
+
+
+  (() => {
+    const refs = {
+      // Додати атрибут data-mob-modal-open на кнопку відкриття
+      openModalBtn: document.querySelector("[data-mob-modal-open]"),
+      // Додати атрибут data-mob-modal-close на кнопку закриття
+      closeModalBtn: document.querySelector("[data-mob-modal-close]"),
+      // Додати атрибут data-modal на бекдроп модалки
+      modal: document.querySelector("[data-mob-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+      refs.modal.classList.toggle("mob-is-open");
+    }
+  })();
